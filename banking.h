7261e50ee0ffd3d70dc58aee5c993f703aa94478
Gillam <41242486+G1llam@users.noreply.github.com>
2023-04-09 21:13:18 +0100

    Update banking.h

--- banking.h
+++ banking.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 int _withdraw {};
 int _deposit {};
@@ -20,48 +21,73 @@ class banking{
         //banking() : ChosenClient(Dave) {}  // using a constructor to set the default value to dave, not really needed for this
 
         void Withdraw(){
-            std::cout << "how much would you like to withdraw? ";
-            std::cin >> _withdraw;
-            *_bal -= _withdraw;
-            std::cout << _withdraw << " has been withdrawn" << std::endl;
+            while(true){
+                std::cout << "how much would you like to withdraw? ";
+                std::cin >> _withdraw;
+                if(_withdraw > *_bal){
+                    std::cout << "You're trying to withdraw a value grater than your balance! \n";
+                    std::cout << "Your balance is: " << *_bal << "\n";
+                }else {
+                    *_bal -= _withdraw;
+                    std::cout << _withdraw << " has been withdrawn" << std::endl;
 
-            Balance();
+                    Balance();
+                    return;
+                }
+            }
         }
 
         void Deposit(){
             std::cout << "how much would you like to deposit? ";
-            std::cin >> _deposit;
-            *_bal += _deposit;
-            std::cout << _deposit << " has been deposited" << std::endl;
-
-            Balance();
+            while (true) {
+                std::cin >> _deposit;
+                if (_deposit >= 0) {
+                    *_bal += _deposit;
+                    std::cout << _deposit << " has been deposited" << std::endl;
+                    Balance();
+                    break;
+                }
+                else {
+                    std::cout << "You entered an invalid amount, Please enter a positive number: ";
+                }
+            }
         }
 
         void Transfer(){  
             std::cout << "Who would you like to transfer money to? ";
             std::cin >> TransferClient;
             std::cout << "How much would you like to transfer to " << TransferClient << "? ";
-            std::cin >> TransferAmount;
 
-            if (TransferClient == "Dave" || TransferClient == "dave") {
-                ChosenClient = Dave;
-            } else if (TransferClient == "Lisa" || TransferClient == "lisa") {
-                ChosenClient = Lisa;
-            } else if (TransferClient == "Alfie" || TransferClient == "alfie") {
-                ChosenClient = Alfie;
-            } else {
-                std::cout << "No such client exists, Please try again!";
-                return;
+            while(true){
+                std::cin >> TransferAmount;
+
+                if (TransferAmount > *_bal) {
+                    std::cout << "Insufficient balance. ";
+                    std::cout << "You have " << *_bal << " available." << std::endl;
+                    std::cout << "Please enter a valid amount: ";
+                } else {
+                    if (TransferClient == "Dave" || TransferClient == "dave") {
+                        ChosenClient = Dave;
+                    } else if (TransferClient == "Lisa" || TransferClient == "lisa") {
+                        ChosenClient = Lisa;
+                    } else if (TransferClient == "Alfie" || TransferClient == "alfie") {
+                        ChosenClient = Alfie;
+                    } else {
+                        std::cout << "No such client exists, Please try again!";
+                        return;
+                    }
+
+                    switch(ChosenClient){
+                        case Dave: std::cout << "Sending "<< TransferAmount << " to Dave.. \n"; break;
+                        case Lisa: std::cout << "Sending "<< TransferAmount << " to Lisa.. \n"; break;
+                        case Alfie: std::cout << "Sending "<< TransferAmount << " to Alfie.. \n"; break;
+                    }
+
+                    *_bal -= TransferAmount;
+                    Balance(); 
+                    return;
+                }       
             }
-
-            switch(ChosenClient){
-                case Dave: std::cout << "Sending "<< TransferAmount << " to Dave.. \n"; break;
-                case Lisa: std::cout << "Sending "<< TransferAmount << " to Lisa.. \n"; break;
-                case Alfie: std::cout << "Sending "<< TransferAmount << " to Alfie.. \n"; break;
-            }
-
-            *_bal -= TransferAmount;
-            Balance(); 
         }
 
         void Balance(){
