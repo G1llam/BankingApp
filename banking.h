1f40ea885ee2abefd30a8873f8e0de69f8bd3ba3
Gillam <41242486+G1llam@users.noreply.github.com>
2023-04-09 18:53:05 +0100

    Update banking.h

--- banking.h
+++ banking.h
@@ -1,17 +1,31 @@
 #include <iostream>
+#include <string>
 
 int _withdraw {};
 int _deposit {};
 int bal {1534};
 int *_bal {&bal};
+std::string TransferClient;
+int TransferAmount {};
+
+enum TransferList{
+    Dave, Lisa, Alfie
+};
 
 class banking{
+    private:
+        TransferList ChosenClient;
+
     public:
+        //banking() : ChosenClient(Dave) {}  // using a constructor to set the default value to dave, not really needed for this
+
         void Withdraw(){
             std::cout << "how much would you like to withdraw? ";
             std::cin >> _withdraw;
             *_bal -= _withdraw;
             std::cout << _withdraw << " has been withdrawn" << std::endl;
+
+            Balance();
         }
 
         void Deposit(){
@@ -19,6 +33,35 @@ class banking{
             std::cin >> _deposit;
             *_bal += _deposit;
             std::cout << _deposit << " has been deposited" << std::endl;
+
+            Balance();
+        }
+
+        void Transfer(){  
+            std::cout << "Who would you like to transfer money to? ";
+            std::cin >> TransferClient;
+            std::cout << "How much would you like to transfer to " << TransferClient << "? ";
+            std::cin >> TransferAmount;
+
+            if (TransferClient == "Dave" || TransferClient == "dave") {
+                ChosenClient = Dave;
+            } else if (TransferClient == "Lisa" || TransferClient == "lisa") {
+                ChosenClient = Lisa;
+            } else if (TransferClient == "Alfie" || TransferClient == "alfie") {
+                ChosenClient = Alfie;
+            } else {
+                std::cout << "No such client exists, Please try again!";
+                return;
+            }
+
+            switch(ChosenClient){
+                case Dave: std::cout << "Sending "<< TransferAmount << " to Dave.. \n"; break;
+                case Lisa: std::cout << "Sending "<< TransferAmount << " to Lisa.. \n"; break;
+                case Alfie: std::cout << "Sending "<< TransferAmount << " to Alfie.. \n"; break;
+            }
+
+            *_bal -= TransferAmount;
+            Balance(); 
         }
 
         void Balance(){
